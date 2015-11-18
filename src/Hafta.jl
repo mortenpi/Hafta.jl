5fd39629f8d25e823b99cf4c767c562ca0a70b29
Morten Piibeleht <morten.piibeleht@gmail.com>
2015-11-18 17:30:02 +0100

    Add ManyBodySystem abstract type

--- src/Hafta.jl
+++ src/Hafta.jl
@@ -1,5 +1,23 @@
 module Hafta
 
+abstract QuantumBasis
+
+"""
+A `ManyBodySystem` is the abstract type that defines an pairwise interacting
+quantum system. It should have the following methods:
+
+  - `Base.size(s::T)`
+  - `H0(s::T, i, j)`
+  - `V(s::T, i, j, k, l)`
+"""
+abstract ManyBodySystem
+
+function H0 end
+export H0
+
+function V end
+export V
+
 include("HarmonicOscillator.jl")
 
 end
