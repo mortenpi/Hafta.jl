cc100548c0ad42a5b0443d383513dbe7d64140d8
Morten Piibeleht <morten.piibeleht@cern.ch>
2016-01-28 15:40:11 +0200

    Move requirements to REQUIRE
    Except the Orthopolys requirement, since that has to be installed
    manually.

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,2 @@
-Pkg.add("Combinatorics")
-Pkg.add("Polynomials")
-Pkg.add("Formatting")
-Pkg.add("Gadfly")
-
 # The Orthopolys packages is not in the package repo
 Pkg.clone("https://github.com/mortenpi/Orthopolys.jl.git")
