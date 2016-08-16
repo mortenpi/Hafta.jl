ec40337417d7082b07db4869ebec599f7299599e
autodocs <autodocs>
2016-08-16 20:45:57 +0000

    build based on 107c674

---
+++ latest/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Hafta.jl-1",
+    "page": "Introduction",
+    "title": "Hafta.jl",
+    "category": "section",
+    "text": "Hartree-Fock and others for quantum systems."
+},
+
+{
+    "location": "api.html#",
+    "page": "Hafta API overview",
+    "title": "Hafta API overview",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "api.html#Hafta-API-overview-1",
+    "page": "Hafta API overview",
+    "title": "Hafta API overview",
+    "category": "section",
+    "text": "A high level overview of structures and functions in the Hafta.jl package."
+},
+
+{
+    "location": "api.html#HarmonicOscillator-1",
+    "page": "Hafta API overview",
+    "title": "HarmonicOscillator",
+    "category": "section",
+    "text": "This module defines things necessary to calculate a 1D harmonic oscillator.It defines two main things:  - Psi function which returns an oscillator basis function.  - generate_wmatrix functions, which generates the W-matrix, which is    basically a lookup table for the delta interaction in 1D.    It returns a WMatrix object and the different elements can be accessed    using the usual index notation: w[i,j,k,l]."
+},
+
+]}
