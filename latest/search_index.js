87b7613daf60ad04ecba0f177a6e85c803eb0d86
autodocs <autodocs>
2016-11-13 18:06:39 +0000

    build based on 8d5ef18

--- latest/search_index.js
+++ latest/search_index.js
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Hafta API overview",
     "title": "HarmonicOscillator",
     "category": "section",
-    "text": "This module defines things necessary to calculate a 1D harmonic oscillator.It defines two main things:Psi function which returns an oscillator basis function.\ngenerate_wmatrix functions, which generates the W-matrix, which is basically a lookup table for the delta interaction in 1D. It returns a WMatrix object and the different elements can be accessed using the usual index notation: w[i,j,k,l]."
+    "text": "This module defines things necessary to calculate a 1D harmonic oscillator.It defines two main things:  - Psi function which returns an oscillator basis function.  - generate_wmatrix functions, which generates the W-matrix, which is    basically a lookup table for the delta interaction in 1D.    It returns a WMatrix object and the different elements can be accessed    using the usual index notation: w[i,j,k,l]."
 },
 
 ]}
