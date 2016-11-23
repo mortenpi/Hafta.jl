90041930a5097a0b774d3bd138db263385e11f5a
Morten Piibeleht <morten.piibeleht@gmail.com>
2016-11-23 13:55:36 +0100

    Use at-testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,29 @@
-using Base.Test
+if VERSION >= v"0.5.0-dev+7720"
+    using Base.Test
+else
+    using BaseTestNext
+    const Test = BaseTestNext
+end
+
 import Hafta
 
-@test Hafta.HarmonicOscillator.wmatrix_arridx_fromsorted(36,46,37,22) == 22907
-@test Hafta.HarmonicOscillator.wmatrix_arridx_fromsorted_recursive(36,46,37,22) == 22907
+@testset "HarmonicOscillator" begin
+    @test Hafta.HarmonicOscillator.wmatrix_arridx_fromsorted(36,46,37,22) == 22907
+    @test Hafta.HarmonicOscillator.wmatrix_arridx_fromsorted_recursive(36,46,37,22) == 22907
 
-# The empty_expression() function needs some tests, just to make sure that
-# it works properly with all versions.
-@test   Hafta.HarmonicOscillator.empty_expression( :() )
-@test ! Hafta.HarmonicOscillator.empty_expression( :(5,6) )
-@test ! Hafta.HarmonicOscillator.empty_expression( :([]) )
-# These fail at the moment because these expression are not Expr objects.
-# See the comment and TOOD at the function definition.
-#@test ! Hafta.HarmonicOscillator.empty_expression( :(x) )
-#@test ! Hafta.HarmonicOscillator.empty_expression( :(5) )
+    # The empty_expression() function needs some tests, just to make sure that
+    # it works properly with all versions.
+    @test   Hafta.HarmonicOscillator.empty_expression( :() )
+    @test ! Hafta.HarmonicOscillator.empty_expression( :(5,6) )
+    @test ! Hafta.HarmonicOscillator.empty_expression( :([]) )
+    # These fail at the moment because these expression are not Expr objects.
+    # See the comment and TOOD at the function definition.
+    #@test ! Hafta.HarmonicOscillator.empty_expression( :(x) )
+    #@test ! Hafta.HarmonicOscillator.empty_expression( :(5) )
 
-# TODO: the basis function definitions should be switched to types probably.
-# Then we'd be storing the polynomial as well and we could check parity with
-# that.
-for i in 0:2:100 @test Hafta.HarmonicOscillator.Psi_parity(i) === 1 end
-for i in 1:2:101 @test Hafta.HarmonicOscillator.Psi_parity(i) === -1 end
+    # TODO: the basis function definitions should be switched to types probably.
+    # Then we'd be storing the polynomial as well and we could check parity with
+    # that.
+    for i in 0:2:100 @test Hafta.HarmonicOscillator.Psi_parity(i) === 1 end
+    for i in 1:2:101 @test Hafta.HarmonicOscillator.Psi_parity(i) === -1 end
+end
